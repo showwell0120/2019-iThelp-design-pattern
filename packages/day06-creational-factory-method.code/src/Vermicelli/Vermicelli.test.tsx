8e0893085e7a7c4f4cd21b6bb3f378b60327c2c6
showwell <showwell0120@gmail.com>
2019-10-24 15:44:08 +0800

    add scss mock

--- packages/day06-creational-factory-method.code/src/Vermicelli/Vermicelli.test.tsx
+++ packages/day06-creational-factory-method.code/src/Vermicelli/Vermicelli.test.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { FatVermicelli } from ".";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { FatVermicelli } from '.';
 
 let container;
 
 //每個測試案例前執行(更換element,Reseting)
 beforeEach(() => {
-  container = document.createElement("div");
+    container = document.createElement('div');
 });
 
 //提取重複程式
@@ -14,9 +14,9 @@ const render = component => ReactDOM.render(component, container);
 
 const form = (id: string) => container.querySelector(`form[id="${id}"]`);
 
-describe("FatVermicelli", () => {
-  it("render FatVermicelli order form", () => {
-    render(<FatVermicelli onSubmit={null} />);
-    expect(form("fatVermicelliOrder")).not.toBeNull();
-  });
+describe('FatVermicelli', () => {
+    it('render FatVermicelli order form', () => {
+        render(<FatVermicelli onSubmit={null} />);
+        expect(form('fatVermicelliOrder')).not.toBeNull();
+    });
 });
--- packages/day08-creational-prototype.code/src/Copier/Copier.test.tsx
+++ packages/day08-creational-prototype.code/src/Copier/Copier.test.tsx
@@ -1,21 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { ReportCopier } from ".";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ReportCopier } from '.';
 
 let container;
 
 //每個測試案例前執行(更換element,Reseting)
 beforeEach(() => {
-  container = document.createElement("div");
+    container = document.createElement('div');
 });
 
 //提取重複程式
 const render = component => ReactDOM.render(component, container);
 const form = (id: string) => container.querySelector(`form[id="${id}"]`);
 
-describe("ReportCopier", () => {
-  it("render ReportCopier", () => {
-    render(<ReportCopier />);
-    expect(form("setting")).not.toBeNull();
-  });
+describe('ReportCopier', () => {
+    it('render ReportCopier', () => {
+        render(<ReportCopier />);
+        expect(form('setting')).not.toBeNull();
+    });
 });
